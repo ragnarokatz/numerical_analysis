0ff71024997618e319b2287999322750e24825bd
rag <ragnarokatz@gmail.com>
2018-11-07 23:49:17 -0500

    add annualization factor in calculation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def generate_date_series():
 
 def generate_date_table():
     end_dates = generate_date_series().to_series()
-    start_dates = [end_dates - pd.DateOffset(years=n) for n in range(1, 11)]
+    start_dates = [end_dates - pd.DateOffset(years=n, days=1) for n in range(1, 11)]
     df = pd.concat(start_dates, axis=1)
     df = df.rename({n: f'{n+1}_year' for n in range(0, 10)}, axis=1)
     return df
@@ -46,7 +46,7 @@ for n in range(1, 11):
     merged = merged.join(joinee, on=f'{n}_year', how='left', rsuffix=f'_{n}_year')
 
 for n in range(1, 11):
-    merged[f'return_{n}_year'] = np.exp(merged['log_cum'] - merged[f'log_cum_{n}_year'])
+    merged[f'return_{n}_year'] = np.exp(merged['log_cum'] - merged[f'log_cum_{n}_year']) ** (1 / n) - 1
 
 merged.to_csv('merged.csv')
 
