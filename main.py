335f849783dc48be7fce30ca960cf3ed8c68d99d
rag <ragnarokatz@gmail.com>
2018-11-07 22:26:20 -0500

    Add initialization of pseudo data tables

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-import timedelta
-
 import pandas as pd
 import numpy as np
 
 
+def generate_date_series():
+    return pd.date_range('1970-01-01', periods=1000)
+
+
 def generate_fundnav_table():
-    rnds = np.random.rand(3,4)
+    rnds = np.random.rand(1000,4)
     df = pd.DataFrame(rnds, columns=list('abcd'))
+    df['trade_date'] = generate_date_series()
     return df
 
 
 def generate_date_table():
-    dr = pd.date_range('1970-01-01', periods=1000)
-    for n in range(1, 10):
-        ts = dr - timedelta(years=n)
+    end_dates = generate_date_series().to_series()
+    start_dates = [end_dates - pd.DateOffset(years=n) for n in range(0, 10)]
+    df = pd.concat(start_dates, axis=1)
+    df = df.rename({n: f'{n}_year' for n in range(0, 10)}, axis=1)
+    return df
 
-    return df(dr, columns='trade_date',)
 
-
-df = generate_fundnav_table()
+fundnavs = generate_fundnav_table()
 dates = generate_date_table()
-df.join(dates, 
+
+print(fundnavs)
+print(dates)
+# import pdb; pdb.set_trace()
+
